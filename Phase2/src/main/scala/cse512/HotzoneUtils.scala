ea8f2ed8b2c2030295f825d1b267ecb8fcf20cd7
Yu-Hsien Tu <cylt2212@gmail.com>
2022-04-19 12:33:15 -0700

    Adjust/ change java math package to scala math package

--- Phase2/src/main/scala/cse512/HotzoneUtils.scala
+++ Phase2/src/main/scala/cse512/HotzoneUtils.scala
@@ -1,9 +1,10 @@
 package cse512
+import scala.math._
 
 object HotzoneUtils {
 
   def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
-    /// Parse Point Query and Convert to Double
+    // Parse Point Query and Convert to Double
     val data = pointString.split(",").map(_.toDouble)
     // Store x and y coordinates of point
     val data_x = data(0)
@@ -11,10 +12,10 @@ object HotzoneUtils {
     // Parse queryRectangle and Convert to Double
     val rectangle = queryRectangle.split(",").map(_.toDouble)
     // Store x and y coordinates of rectangle
-    val rect_x1 = Math.min(rectangle(0), rectangle(2))
-    val rect_y1 = Math.min(rectangle(1),rectangle(3))
-    val rect_x2 = Math.max(rectangle(0), rectangle(2))
-    val rect_y2 = Math.max(rectangle(1),rectangle(3))
+    val rect_x1 = min(rectangle(0), rectangle(2))
+    val rect_y1 = min(rectangle(1),rectangle(3))
+    val rect_x2 = max(rectangle(0), rectangle(2))
+    val rect_y2 = max(rectangle(1),rectangle(3))
     // Check if point is within the rectangle
     return (data_x >= rect_x1 && data_x <= rect_x2 && data_y >= rect_y1 && data_y <= rect_y2)
   }
