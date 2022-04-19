8f229c1d6e4b1f4cdd274783dbd3ae0b53c2fcbe
Yu-Hsien Tu <cylt2212@gmail.com>
2022-04-19 12:47:32 -0700

    Adjust/ toDouble syntax

--- Phase2/src/main/scala/cse512/HotzoneUtils.scala
+++ Phase2/src/main/scala/cse512/HotzoneUtils.scala
@@ -5,17 +5,17 @@ object HotzoneUtils {
 
   def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
     // Parse Point Query and Convert to Double
-    val data = pointString.split(",").map(_.toDouble)
+    val data = pointString.split(",")
     // Store x and y coordinates of point
-    val data_x = data(0)
-    val data_y = data(1)
+    val data_x = data(0).toDouble
+    val data_y = data(1).toDouble
     // Parse queryRectangle and Convert to Double
-    val rectangle = queryRectangle.split(",").map(_.toDouble)
+    val rectangle = queryRectangle.split(",")
     // Store x and y coordinates of rectangle
-    val rect_x1 = min(rectangle(0), rectangle(2))
-    val rect_y1 = min(rectangle(1),rectangle(3))
-    val rect_x2 = max(rectangle(0), rectangle(2))
-    val rect_y2 = max(rectangle(1),rectangle(3))
+    val rect_x1 = min(rectangle(0).toDouble, rectangle(2).toDouble)
+    val rect_y1 = min(rectangle(1).toDouble, rectangle(3).toDouble)
+    val rect_x2 = max(rectangle(0).toDouble, rectangle(2).toDouble)
+    val rect_y2 = max(rectangle(1).toDouble, rectangle(3).toDouble)
     // Check if point is within the rectangle
     return (data_x >= rect_x1 && data_x <= rect_x2 && data_y >= rect_y1 && data_y <= rect_y2)
   }
